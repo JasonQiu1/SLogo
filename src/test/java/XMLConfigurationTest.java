93c185f2c76f36babe9f1197ea6f7da23e5bdd86
Jordan Haytaian <jeh120@duke.edu>
2024-02-25 20:44:24 -0500

    XML configuration test

---
+++ src/test/java/XMLConfigurationTest.java
@@ -0,0 +1,25 @@
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import slogo.model.XMLConfiguration;
+
+public class XMLConfigurationTest {
+
+  private XMLConfiguration xmlConfig;
+  @BeforeEach
+  void setup() {
+    xmlConfig = new XMLConfiguration();
+  }
+
+  @Test
+  void testLoadHelpFile() {
+  }
+
+  @Test
+  void testLoadSession() {
+  }
+
+  @Test
+  void testSaveSession() {
+  }
+}
