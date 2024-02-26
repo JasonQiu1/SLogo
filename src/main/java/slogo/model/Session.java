a467670664540aab49e056ffc012e642aa46c0a5
Jason Qiu <jq48@duke.edu>
2024-02-25 23:37:06 -0500

    feat [DESIGN-08]: updated Session boilerplate api and documentation

--- src/main/java/slogo/model/Session.java
+++ src/main/java/slogo/model/Session.java
@@ -1,12 +1,12 @@
 package slogo.model;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import slogo.model.coderunner.RunCodeError;
 import slogo.model.coderunner.SlogoCodeRunner;
 import slogo.model.turtle.Turtle;
+import slogo.model.turtle.TurtleState;
 import slogo.model.turtle.TurtleStep;
 
 /**
@@ -28,34 +28,45 @@ public class Session {
    * Runs lines of code, affecting command history and step history for each turtle.
    *
    * @param commands the raw code, with each line of code on a different element
-   * @throws RunCodeError when XXXXXX
+   * @throws RunCodeError when any error occurs when running the code.
    */
   public void run(String commands) throws RunCodeError {
     return;
   }
 
   /**
-   * Returns the current command history of a certain length. If length=-1, then returns entire
-   * command history.
+   * Returns the current command history of a certain length.
    *
-   * @param length the max length of command history to return. If -1, then return entire command
-   *               history.
-   * @return an immutable list of maps where the key is the command and the value is another map of metadata
-   * such as wasSuccessful.
+   * @param maxLength the max length of command history to return. If 0, then return entire command
+   *                  history (including forward history). If positive, then return the backwards
+   *                  history up to length. If negative, then return the forwards history up to
+   *                  -length.
+   * @return an immutable list of maps where the key is the command and the value is another map of
+   * metadata such as wasSuccessful.
    */
-  public List<Map<String, Map<String, String>>> getCommandHistory(int length) {
+  public List<Map<String, Map<String, String>>> getCommandHistory(int maxLength) {
     return null;
   }
 
   /**
-   * Returns the current step history for all turtles. If length=-1, then returns entire command
-   * history.
+   * Returns the current step history of a certain length for all turtles.
    *
-   * @param length the max length of each turtle's step history to return. If -1, then return entire
-   *               step history of each turtle.
+   * @param maxLength the max length of step history to return. If 0, then return entire step
+   *                  history (including forward history). If positive, then return the backwards
+   *                  history up to length. If negative, then return the forwards history up to
+   *                  -length.
    * @return an immutable map where the key is the id of the turtle and a list of its step history
    */
-  public Map<Integer, List<TurtleStep>> getTurtlesStepHistory(int length) {
+  public Map<Integer, List<TurtleStep>> getTurtlesStepHistories(int maxLength) {
+    return null;
+  }
+
+  /**
+   * Returns the current state for each turtle.
+   *
+   * @return an immutable map where the key is the id of the turtle and the value is its state
+   */
+  public Map<Integer, TurtleState> getTurtlesCurrentStates() {
     return null;
   }
 
@@ -87,27 +98,27 @@ public class Session {
    * metadata, including arity and command definition.
    */
 
-  public Map<String, String> getLibraryCommands() {
+  public Map<String, Map<String, String>> getLibraryCommands() {
     return null;
   }
 
   /**
-   * Steps command history and each turtle's step history back a number of steps.
+   * Steps (successful) command history and each turtle's step history back a number of steps.
    *
-   * @param numSteps how many steps to go back. If -1, then go back to initial state.
+   * @param maxSteps the maximum number of steps to take. If negative, then will redo instead.
    * @return true if there was enough history to go back numSteps, false otherwise.
    */
-  public boolean undo(int numSteps) {
+  public boolean undo(int maxSteps) {
     return false;
   }
 
   /**
-   * Steps command history and each turtle's step history forward a number of steps.
+   * Steps (successful) command history and each turtle's step history forward a number of steps.
    *
-   * @param numSteps how many steps to go forward. If -1, then go all the way to furthest state.
-   * @return true if there was enough forward history to go forward numSteps, false otherwise.
+   * @param maxSteps the maximum number of steps to take. If negative, then will undo instead.
+   * @return true if there was enough forwardhistory to go forward numSteps, false otherwise.
    */
-  public boolean redo(int numSteps) {
+  public boolean redo(int maxSteps) {
     return false;
   }
 
