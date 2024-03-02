6a50bbd8b3e9ce5d385bec060c86e18375a6a8c9
Jason Qiu <jq48@duke.edu>
2024-03-02 13:46:34 -0500

    feat: implemented interpreter functionality for all expressions not dependent on the environment and tests for them all

--- src/main/java/slogo/model/coderunner/Interpreter.java
+++ src/main/java/slogo/model/coderunner/Interpreter.java
@@ -1,46 +1,79 @@
 package slogo.model.coderunner;
 
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import slogo.model.api.exception.coderunner.ErrorType;
+import slogo.model.api.exception.coderunner.RunCodeError;
 import slogo.model.coderunner.Expression.Binary;
 import slogo.model.coderunner.Expression.Call;
 import slogo.model.coderunner.Expression.DefineCommand;
-import slogo.model.coderunner.Expression.Logical;
 import slogo.model.coderunner.Expression.Number;
 import slogo.model.coderunner.Expression.SetVariable;
 import slogo.model.coderunner.Expression.Unary;
 import slogo.model.coderunner.Expression.Variable;
+import slogo.model.coderunner.Expression.Visitor;
 
 /**
  * Executes abstract syntax trees (statements and expressions).
  *
  * @author Jason Qiu
  */
-class Interpreter implements Expression.Visitor {
+class Interpreter implements Visitor {
 
-  private Environment _globalEnvironment;
-  private Environment _currentEnvironment;
-
-  void evaluate(Expression expression) {
-    expression.accept(this);
+  public double evaluate(Expression expression) {
+    return expression.accept(this);
   }
 
   @Override
   public double visitNumber(Number expression) {
-    return 0;
+    return expression.getValue();
+  }
+
+  private BiFunction<Double, Double, Double> getBinaryOperator(Token operatorToken) {
+    return switch (operatorToken.type()) {
+      case PLUS -> Double::sum;
+      case MINUS -> (a, b) -> a - b;
+      case STAR -> (a, b) -> a * b;
+      case FORWARD_SLASH -> (a, b) -> {
+        if (b == 0) {
+          throw createError("divideByZero", operatorToken);
+        }
+        return (a / b);
+      };
+      case PERCENT -> (a, b) -> {
+        if (b == 0) {
+          throw createError("moduloByZero", operatorToken);
+        }
+        return (a % b);
+      };
+      case EQUAL_TO -> (a, b) -> booleanToDouble(a.equals(b));
+      case NOT_EQUAL_TO -> (a, b) -> booleanToDouble(!a.equals(b));
+      case GREATER_THAN -> (a, b) -> booleanToDouble(a > b);
+      case LESS_THAN -> (a, b) -> booleanToDouble(a < b);
+      case GREATER_EQUAL_TO -> (a, b) -> booleanToDouble(a >= b);
+      case LESS_EQUAL_TO -> (a, b) -> booleanToDouble(a <= b);
+      default -> throw createError("invalidBinaryOperator", operatorToken);
+    };
   }
 
   @Override
   public double visitBinary(Binary expression) {
-    return 0;
+    BiFunction<Double, Double, Double> binaryOperator = getBinaryOperator(expression.getOperator());
+    return binaryOperator.apply(evaluate(expression.getLeft()), evaluate(expression.getRight()));
   }
 
-  @Override
-  public double visitLogical(Logical expression) {
-    return 0;
+  private Function<Double, Double> getUnaryOperator(Token operatorToken) {
+    return switch (operatorToken.type()) {
+      case TILDA -> (x) -> (x == 0) ? 0 : -x;
+      default -> throw new RunCodeError(ErrorType.PARSE, "invalidBinaryOperator",
+          operatorToken.lineNumber(), operatorToken.line());
+    };
   }
 
   @Override
   public double visitUnary(Unary expression) {
-    return 0;
+    Function<Double, Double> unaryOperator = getUnaryOperator(expression.getOperator());
+    return unaryOperator.apply(evaluate(expression.getRight()));
   }
 
   @Override
@@ -61,5 +94,17 @@ class Interpreter implements Expression.Visitor {
   @Override
   public double visitCall(Call expression) {
     return 0;
+  }
+
+
+  private Environment _globalEnvironment;
+  private Environment _currentEnvironment;
+
+  private double booleanToDouble(boolean bool) {
+    return bool ? 1 : 0;
+  }
+
+  private RunCodeError createError(String messageKey, Token token) {
+    return new RunCodeError(ErrorType.INTERPRET, messageKey, token.lineNumber(), token.line());
   }
 }
