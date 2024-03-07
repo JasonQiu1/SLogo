15cd073e70823f0c1a53faa04a2e0248db1c46ce
Judy He <yh381@duke.edu>
2024-03-06 21:04:28 -0500

    [SLOGO 74] Implemented View side of turtle animation, integrated with Model

--- src/main/java/slogo/controller/controllers/TurtleController.java
+++ src/main/java/slogo/controller/controllers/TurtleController.java
@@ -3,13 +3,20 @@ package slogo.controller.controllers;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.util.Collection;
-import java.util.InputMismatchException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.util.Duration;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.TransformerException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import slogo.model.api.turtle.TurtleState;
+import slogo.model.api.turtle.TurtleStep;
 import slogo.view.userinterface.UIButton;
 import slogo.view.userinterface.UIElement;
 import slogo.view.userinterface.UITextField;
@@ -19,14 +26,19 @@ import slogo.view.userinterface.UITurtle;
  * TurtleController class implements UIController interface to manage turtle UI elements. It
  * provides functionality to control the movement and appearance of the turtle.
  *
- * @author Jeremyah Flowers
+ * @author Jeremyah Flowers, Judy He
  */
 public class TurtleController extends UIController {
 
     // Instance Variables
     public static final String TURTLE_XML = "src/main/resources/selected_turtle.xml";
-    private String lastText = "";
-    private double nextY, nextHeading;
+    private final Map<String, UITurtle> TURTLE_VIEWS = new HashMap<>();
+    private Timeline animation;
+    private static final double SECOND_DELAY = 1;
+    private Map<Integer, TurtleState> currentFrame;
+    private int numCommands;
+//    private String lastText = "";
+//    private double nextY, nextHeading;
 
     /**
      * Notifies the turtle controller about changes in UI elements.
@@ -36,59 +48,29 @@ public class TurtleController extends UIController {
     @Override
     public void notifyController(UIElement element) {
         switch (element.getType().toLowerCase()) {
-            case "textfield" -> setMovement((UITextField) element);
+            case "textfield" -> runCommands((UITextField) element);
             case "button" -> handleButtonInput((UIButton) element);
         }
         updateElements();
     }
 
     // Private helper methods
+    private void setAnimation() {
+        animation = new Timeline();
+        animation.setCycleCount(Timeline.INDEFINITE);
+        double frameDuration = 1 / (this.getTurtleAnimator().STANDARD_FPS); // Calculate the duration for the KeyFrame
 
-    private void setMovement(UITextField textField) {
-        String[] newestText = textField.getText();
-        for (String newText : newestText) {
-            parseTxt(newText);
-        }
+        animation.getKeyFrames()
+            .add(new KeyFrame(Duration.seconds(frameDuration), e -> animateTurtleViews()));
+        animation.play();
     }
 
-    private void parseTxt(String text) {
-        switch (text) {
-            case "fd" -> nextY = 1;
-            case "bk" -> nextY = -1;
-            case "rt" -> nextHeading = 1;
-            case "lt" -> nextHeading = -1;
-            default -> setNumeric(text);
-        }
-        lastText = text;
-    }
-
-    private void setNumeric(String text) {
-        if (isNumeric(text)) {
-            double parsedNumber = Double.parseDouble(text);
-            switch (lastText) {
-                case "fd", "bk" -> {
-                    nextY *= parsedNumber;
-                }
-                case "rt", "lt" -> {
-                    nextHeading *= parsedNumber;
-                }
-            }
-        } else {
-            throw new InputMismatchException(
-                    "Command not found. Please read help documentation or try again.");
-        }
-    }
-
-    private boolean isNumeric(String str) {
-        if (str == null) {
-            return false;
-        }
-        try {
-            double d = Double.parseDouble(str);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
+    private void runCommands(UITextField textFieldView) {
+        this.startNewSession();
+        String commands = textFieldView.getTextCommands();
+        // this.numCommands = this.getCurrentSession().run(commands);
+        this.numCommands = 6;
+        this.getCurrentSession().run(commands);
     }
 
     private void updateElements() {
@@ -117,13 +99,41 @@ public class TurtleController extends UIController {
 
     private void updateElement(UIElement element) {
         switch (element.getType().toLowerCase()) {
-            case "textfield" -> setMovement((UITextField) element);
-            case "turtle" -> changePosition((UITurtle) element);
+            case "textfield" -> resetTextField((UITextField) element);
+            case "turtle" -> animateTurtleViews((UITurtle) element);
         }
     }
 
-    private void changePosition(UITurtle turtle) {
-        turtle.createAnimation(0, nextY, nextHeading);
-        turtle.updatePosition();
+    private void animateTurtleViews(UITurtle turtleView) {
+        addTurtleView(turtleView);
+        updateTurtleViews();
+        setAnimation();
     }
+
+    private void resetTextField(UITextField textFieldView) {
+        textFieldView.reset();
+    }
+
+    private void addTurtleView(UITurtle turtleView) {
+        this.TURTLE_VIEWS.put(turtleView.getID(), turtleView);
+    }
+
+    private void updateTurtleViews() {
+        Map<Integer, List<TurtleStep>> totalSteps = this.getCurrentSession().getTurtlesStepHistories(numCommands);
+        this.getTurtleAnimator().animateStep(totalSteps);
+        this.currentFrame = this.getTurtleAnimator().nextFrame();
+    }
+
+    private void animateTurtleViews() {
+        if (this.currentFrame.isEmpty()) {
+            return;
+        }
+        for (Integer turtleId : this.currentFrame.keySet()) {
+            TurtleState state = this.currentFrame.get(turtleId);
+            UITurtle turtleView = this.TURTLE_VIEWS.get(String.valueOf(turtleId));
+            turtleView.updateState(state.position().getX(), state.position().getY(), state.heading());
+        }
+        this.currentFrame = this.getTurtleAnimator().nextFrame();
+    }
+
 }
