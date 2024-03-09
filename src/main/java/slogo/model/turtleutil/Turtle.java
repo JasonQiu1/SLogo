ea59209352dc2a7fba4370500638e1517826ec8d
Judy He <yh381@duke.edu>
2024-03-09 02:21:02 -0500

    fix: Debugged Turtle

--- src/main/java/slogo/model/turtleutil/Turtle.java
+++ src/main/java/slogo/model/turtleutil/Turtle.java
@@ -90,6 +90,8 @@ public class Turtle {
       }
     }
 
+    if (startInd == currentPointInStepHistory - 1) startInd = currentPointInStepHistory - maxLength;
+
     for (int i = startInd; i < this.stepHistory.size(); i++) {
       history.add(this.stepHistory.get(i).turtleStep());
     }
